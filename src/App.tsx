35bec36efe0437a01656db4c01fefeb293f7dc2d
dibya.borah <dibyajyotiborah469@gmail.com>
2024-05-27 20:27:54 +0530

    Added validation using react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,36 @@ import { SubmitHandler, useForm } from "react-hook-form"
 
 type FormFields = {
   email: string,
-  passord: string
+  password: string
 }
 const App = () => {
-  const { register, handleSubmit } = useForm<FormFields>();
-  const onSubmit:SubmitHandler<FormFields> = (data) => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormFields>();
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
     console.log(data);
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <input
-          {...register('email')}
+          {...register('email', {
+            required: 'Email must include @',
+            validate: (value) => value.includes('@')
+          })}
           type="text"
           placeholder="Email"
         />
+        {errors.email && <div style={{color: 'red'}}>{errors.email.message}</div>}
       </div>
       <div>
         <input
-          {...register('passord')}
+          {...register('password', {
+            required: 'Password length should be > 8',
+            minLength: 8,
+          })}
           type="password"
           placeholder="Password"
-        />
+          />
+          {errors.password && <div style={{color: 'red'}}>{errors.password.message}</div>}
       </div>
       <button type="submit">Submit</button>
     </form>
